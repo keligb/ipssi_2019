8f337c00b9806e5f34b858b23689b8b4936f2a9b
keligb <keligbrindeau@gmail.com>
2019-12-06 16:55:54 +0100

    ajout de exam3

--- exam3/noel.py
+++ exam3/noel.py
@@ -1,18 +1,25 @@
-from datetime import datetime
-from datetime import date
+#!/usr/bin/python3
+from datetime import date, datetime
+from tree import show_tree
 import calendar
 import sys
-
-#noel = datetime(2019,12,25)
-#now = date.today()
-
-def show_noel(date_arg=""):
-    noel = datetime(2019,12,25)
-    #now = date.today()
-    date = datetime.strptime(date_arg, '%Y-%m-%d')
-    jours = (noel - date)
-    print(jours)
-
+def show_noel(d):
+    if len(d) == 1:
+        d = date.today()
+    else:
+        d = d[1]
+        d = datetime.strptime(d, '%Y-%m-%d').date()
+    if d.day > 25 and d.month == 12:
+        noel = date(d.year + 1, 12, 25)
+    else:
+        noel = date(d.year, 12, 25)
+        
+    jours_noel = noel - d
+    
+        
+    if noel == d:
+        return(show_tree(10))
+    return jours_noel.days
 if __name__ == "__main__":
     print(show_noel(sys.argv))
     
